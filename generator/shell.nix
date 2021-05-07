70e916cc2a3c79f9e380d0e93d3008b607c60655
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2021-05-07 16:09:38 +0200

    chore: pinned nixpkgs

--- generator/shell.nix
+++ generator/shell.nix
@@ -1,4 +1,10 @@
-{ pkgs ? (import <nixpkgs> {})}:
+{ pkgs ? (import (builtins.fetchTarball {
+  name = "nixos-unstable-2018-09-12";
+  url = "https://github.com/nixos/nixpkgs/archive/32f7980afb5e33f1e078a51e715b9f102f396a69.tar.gz";
+  sha256 = "02k9xnkrcmkr21b8pycmf1rbbnzh2x5i2wj9wmlghijfvpjqdn27";
+}) {})}:
+
+
 pkgs.mkShell {
   buildInputs = [
     pkgs.python39
